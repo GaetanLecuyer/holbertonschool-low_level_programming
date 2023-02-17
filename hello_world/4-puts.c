21bbd7bfefddd19052289d02daaa32a24465c010
GaetanLecuyer <6229@holbertonstudents.com>
2023-02-17 04:36:26 -0800

    steep 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
-#include<studio.h>
+#include <stdio.h>
 
 int main(void)
 {
-  printf("Programming is like building a multilingual puzzle")
-      return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
