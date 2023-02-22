32ffc633504753391ee05f91805ac5d42071a41c
GaetanLecuyer <6229@holbertonstudents.com>
2023-02-22 05:45:15 -0800

    steep 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,26 +1,19 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * print_alphabet_x10 - print alphabet 10 times
- *
- * Return: void
- */
-
+*print_alphabet_x10 - prints alphabet letters
+* ten times
+*desc: The function uses loop to operate
+*Return: no return
+*/
 void print_alphabet_x10(void)
 {
-	int round = 0;
-	char letter = 'a';
+int counter = 0;
+int letter;
+while (counter++ <= 9)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
 
-	while (round < 10)
-	{
-		letter = 'a';
-		while (letter <= 'z')
-		{
-			_putchar(letter);
-			letter++;
-		}
-		_putchar('\n');
-
-		round++;
-	}
+_putchar(10);
+}
 }
