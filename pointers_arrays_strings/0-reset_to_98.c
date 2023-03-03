3a2b212029f56f6121cea668d31546faacec936a
GaetanLecuyer <6229@holbertonstudents.com>
2023-03-03 02:18:38 -0800

    steep 0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,11 @@
 #include "main.h"
+
 /**
- *programe by gaetean
- *reset value of an integer to 98
- *retourn nothing
+ * reset_to_98 - change the value of integer using pointer
+ * @n: pointer
+ * Return:void
  */
 void reset_to_98(int *n)
 {
-  *n=98;
+	*n = 98;
 }
