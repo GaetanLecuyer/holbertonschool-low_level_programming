42b204c02bcc987f71c97a7365abd632ba7f2948
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-03-06 13:18:59 +0100

    corection

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,18 +1,19 @@
 #include "main.h"
-
 /* programe by gaetan */
-/* main.h is a librery */ 
+/* main.h is a librery */
 /* vid _puts_recursion(char *s) is a fonction */
 /* s is a poiinter */
 
 
 void _puts_recursion(char *s)
 {
-    if(*s){
+    if(*s)
+    {
         _putchar(*s);
         _puts_recursion(s + 1);
     }
-    else{
+    else
+    {
         _putchar('\n');
     }
 }
