29a9c9d2aae2224be9f34d296bd100a3efd7422a
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-03-06 12:04:25 +0100

    steep 1

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*programe by gaetan
+*main.h is a librery 
+*oid _puts_recursion(char *s) is a fonction 
+*s is a poiinter 
+*/
+void _puts_recursion(char *s)
+{
+    if(*s)
+    {
+        _putchar(*s);
+        _puts_recursion(s + 1);
+    }
+    else{
+        _putchar('\n');
+    }
+}
