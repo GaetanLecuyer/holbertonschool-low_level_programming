21ddfbab4a824300e653a12ae7721cb6e858b5fc
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-03-07 10:20:57 +0100

    essai

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,2 +1,21 @@
 #include "main.h"
 /*librert to declare in fonction*/
+
+
+int factorial(int n)
+{
+    if (n < 0)
+{
+                return(-1);
+}
+     else if (n == O)
+{
+               return(1);
+}
+    else 
+{
+                    int mul;
+                    mul = factorial(n - 1);
+                    return(n * mul);
+}  
+}
--- recursion/main.h
+++ recursion/main.h
@@ -10,6 +10,7 @@
 
 #include <stddef.h>
 
+int factorial(int n);
 int _putchar(char c);
 void *_memset(void *s, int c, size_t n);
 char *_memcpy(char *dest, char *src, unsigned int n);
