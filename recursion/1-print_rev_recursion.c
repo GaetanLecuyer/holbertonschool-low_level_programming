717e7bb71e93d5e6ffa228912a578682b0570080
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-03-10 14:41:55 +0100

    betty correction

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,14 +1,19 @@
-#include "main.h"
-/*programe by gaetan*/
-/*main.h is a bibliotheque*/
-/*s is a poiteur that lead a main of betty */
-/*fonction void _print_rev_recursion(char *s) is prints a carracte is reverse*/
+/*
+ * File: 1-print_rev_recursion.c
+ * Auth: Lecuyer gaetan
+ */
 
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to be printed.
+ */
 void _print_rev_recursion(char *s)
 {
-if (*s)
-{
-_print_rev_recursion(s+1);
-_putchar(*s);
-}
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
