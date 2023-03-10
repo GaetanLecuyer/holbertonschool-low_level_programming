73d1664b59143b02eaaf8f2a98c9278bda3229ca
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-03-10 14:45:26 +0100

    corection betty

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,19 +1,22 @@
+/*
+ * File: 0-puts_recursion.c
+ * Auth: Brennan D Baraban
+ */
+
 #include "main.h"
-/* programe by gaetan */
-/* main.h is a librery */
-/* vid _puts_recursion(char *s) is a fonction */
-/* s is a poiinter */
 
-
+/**
+ * _puts_recursion - Prints a string, followed by a new line.
+ * @s: The string to be printed.
+ */
 void _puts_recursion(char *s)
 {
-if (*s) 
-{
-_putchar(*s);
-_puts_recursion(s + 1);
-}
-else
-{
-_putchar('\n');
-}
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
 }
