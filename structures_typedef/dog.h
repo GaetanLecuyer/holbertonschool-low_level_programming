49dbd4c9302556744a4140f18681350b000d32a5
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-03-23 20:54:49 +0100

    corecition betty

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,13 +1,19 @@
-#ifndef _HEADER_
-#define _HEADER_
+
+
+#ifndef DOG_H
+#define DOG_H
+
+/*
+ * File: dog.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file that defines a new type struct dog.
+ */
 
 /**
- * struct dog - Dog attributes
+ * struct dog - A new type describing a dog.
  * @name: The name of the dog.
  * @age: The age of the dog.
  * @owner: The owner of the dog.
- *
- * Description: The attributes of a dog.
  */
 struct dog
 {
@@ -16,11 +22,10 @@ struct dog
 	char *owner;
 };
 
-typedef struct dog dog_t;
-
 /**
- * dog_t - Typedef for dog structure
+ * dog_t - Typedef for struct dog
  */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
