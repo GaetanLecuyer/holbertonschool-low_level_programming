13ee040c5bdcf404eb8f3c8efcb3d96661b56394
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-04-04 14:37:09 +0200

    create file .h

---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,29 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/*
+ * File: lists.h
+ * Desc: Header file containing prototypes and definitions for all functions
+ *       and types written in the 0x11-singly_linked_lists directory.
+ */
+
+#include <stdlib.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *              for Holberton project
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+ize_t print_list(const list_t *h);
+#endif
