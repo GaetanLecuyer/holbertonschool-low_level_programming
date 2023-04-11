c73165df06cab5c29f274533e39c5150ead9b7b3
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-04-11 16:18:10 +0200

    test in the cheacker

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    int fd;
+    int r, w;
+    char *buf;
+
+    if(!filename)
+        return(0);
+    fd = open(filename, O_RDONLY);
+    if (fd < 0)
+        return(0);
+
+    buf = malloc(sizeof(char) * letters);
+    r = read(fd, buf, letters);
+    
+    if (r < 0)
+    {
+        free(buf);
+        return(0);
+    }
+    buf[r]= '\0';
+    close(fd);
+    w = write(STDOUT_FILENO, buf, r);
+    if (w < 0)
+    {
+        free(buf);
+        return(0);
+    }
+    free(buf);
+    return(w);
+}
