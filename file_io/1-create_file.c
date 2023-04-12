e251c28b73b069fc5520db245275aa73f43b4ca1
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-04-12 15:33:44 +0200

    corection

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,14 +1,8 @@
 #include "main.h"
 
 
-int _strlen(char *s)
-{
-  int a = 0;
 
-  while (*s++)
-    a++;
-  return (a);
-}
+
 int create_file(const char *filename, char *text_content)
 {
   int fd;
