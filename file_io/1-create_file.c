a6e57b55b9ee888c27f9d21fbcc6d736a3927b3f
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-04-12 15:41:42 +0200

    corection and redius ligne of code

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,36 +1,22 @@
 #include "main.h"
-
+#include <string.h>
 
 int create_file(const char *filename, char *text_content)
 {
   int fd;
-  ssize_t len;
-  
-  if (filename == NULL)
-  {
-    return (-1);
-  }
-
-  fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC | 0644);
-
-  if (fd == -1)
-  {
-    write(STDERR_FILENO, "fails\n", 6);
-    return (-1);
-  }
+  char *buf;
+  size_t count;
 
   if (text_content != NULL)
   {
-    len = write(fd, text_content,_strlen(text_content));
-    if (len == -1)
-    {
-      write(STDERR_FILENO, "fails\n", 6);
-      close(fd);
-      return (-1);
-
-    }
+    buf = text_content;
+    count = strlen(buf);
   }
-  close(fd);
+
+  fd = creat(filename, S_IRUSR | S_IWUSR);
+  if (fd == -1)
+    return (-1);
+  write(fd, buf, count);
+
   return (1);
 }
-
