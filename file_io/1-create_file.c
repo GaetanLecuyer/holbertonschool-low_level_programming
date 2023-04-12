ba5a14f0268f70a9b8447a0d611d92c34466512c
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-04-12 15:35:58 +0200

    corection

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,8 +1,6 @@
 #include "main.h"
 
 
-
-
 int create_file(const char *filename, char *text_content)
 {
   int fd;
@@ -35,3 +33,4 @@ int create_file(const char *filename, char *text_content)
   close(fd);
   return (1);
 }
+
