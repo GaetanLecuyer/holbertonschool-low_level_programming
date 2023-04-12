b2209bd154ab946aa7697e149340f42077793171
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-04-12 15:28:49 +0200

    corection

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -23,6 +23,7 @@ int create_file(const char *filename, char *text_content)
 
   if (fd == -1)
   {
+    write(STDERR_FILENO, "fails\n", 6);
     return (-1);
   }
 
@@ -31,8 +32,10 @@ int create_file(const char *filename, char *text_content)
     len = write(fd, text_content,_strlen(text_content));
     if (len == -1)
     {
+      write(STDERR_FILENO, "fails\n", 6);
       close(fd);
       return (-1);
+
     }
   }
   close(fd);
